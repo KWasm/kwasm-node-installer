2deefbb8b3667d3f0b828dec50818ac558dc7d51
Sven Pfennig <s.pfennig@reply.de>
2022-10-12 23:27:39 +0200

    add minikube support
    Signed-off-by: Sven Pfennig <s.pfennig@reply.de>

--- script/installer.sh
+++ script/installer.sh
@@ -1,17 +1,21 @@
 #!/usr/bin/env sh
 set -euo pipefail
 
+mkdir -p $NODE_ROOT/opt/kwasm/bin/
+mkdir -p $NODE_ROOT/opt/kwasm/lib/
 case $1 in
     wasmtime)
-        cp /assets/crun-wasmtime $NODE_ROOT/usr/local/bin/crun
-        cp /assets/libwasmtime.so $NODE_ROOT/usr/local/lib/libwasmtime.so
+        cp /assets/crun-wasmtime $NODE_ROOT/opt/kwasm/bin/crun && \
+        cp /assets/libwasmtime.so $NODE_ROOT/opt/kwasm/lib/libwasmtime.so && \
+        ln -sf /opt/kwasm/lib/libwasmtime.so $NODE_ROOT/lib/libwasmtime.so
         ;;
     *)
     #wasmedge)
-        cp /assets/crun-wasmedge $NODE_ROOT/usr/local/bin/crun
-        cp /assets/libwasmedge.so $NODE_ROOT/usr/local/lib/libwasmedge.so
-        ln -s /usr/local/lib/libwasmedge.so $NODE_ROOT/usr/local/lib/libwasmedge.so.0 && \
-        ln -s /usr/local/lib/libwasmedge.so $NODE_ROOT/usr/local/lib/libwasmedge.so.0.0.0
+        cp /assets/crun-wasmedge $NODE_ROOT/opt/kwasm/bin/crun && \
+        cp /assets/libwasmedge.so $NODE_ROOT/opt/kwasm/lib/libwasmedge.so && \
+        ln -sf /opt/kwasm/lib/libwasmedge.so $NODE_ROOT/lib/libwasmedge.so && \
+        ln -sf /opt/kwasm/lib/libwasmedge.so $NODE_ROOT/lib/libwasmedge.so.0 && \
+        ln -sf /opt/kwasm/lib/libwasmedge.so $NODE_ROOT/lib/libwasmedge.so.0.0.0
         ;;
 
 esac
@@ -21,9 +25,13 @@ if ! grep -q crun $NODE_ROOT/etc/containerd/config.toml; then
     runtime_type = "io.containerd.runc.v2"
     pod_annotations = ["module.wasm.image/variant", "run.oci.handler"]
 [plugins."io.containerd.grpc.v1.cri".containerd.runtimes.crun.options]
-    BinaryName = "crun"' >> $NODE_ROOT/etc/containerd/config.toml
+    BinaryName = "/opt/kwasm/bin/crun"' >> $NODE_ROOT/etc/containerd/config.toml
+    rm -Rf $NODE_ROOT/opt/kwasm/active
 fi
 
-nsenter -m/$NODE_ROOT/proc/1/ns/mnt -- ldconfig
-nsenter -m/$NODE_ROOT/proc/1/ns/mnt -- /bin/systemctl restart containerd
-sleep 10
+if [ ! -f $NODE_ROOT/opt/kwasm/active ]; then
+    nsenter -m/$NODE_ROOT/proc/1/ns/mnt -- /bin/systemctl restart containerd
+    touch $NODE_ROOT/opt/kwasm/active
+else
+    echo "No change in containerd/config.toml"
+fi
