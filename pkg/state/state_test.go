e101ad8be98eefb7ba1748025dd6fb2616a4bd7d
Daniel Aberger <da@ixab.de>
2024-02-28 16:39:11 +0100

    add tests for state.ShimChanged

--- pkg/state/state_test.go
+++ pkg/state/state_test.go
@@ -46,3 +46,60 @@ func TestGet(t *testing.T) {
 		})
 	}
 }
+
+func TestShimChanged(t *testing.T) {
+	state := &state{
+		Shims: map[string]*Shim{
+			"spin-v1": {
+				Sha256: []byte{109, 165, 232, 241, 122, 155, 250, 156, 176, 76, 242, 44, 135, 182, 71, 83, 148, 236, 236, 58, 244, 253, 195, 55, 247, 45, 109, 191, 51, 25, 234, 82},
+				Path:   "/opt/kwasm/bin/containerd-shim-spin-v1",
+			},
+		},
+	}
+
+	tests := []struct {
+		name       string
+		shimName   string
+		sha256     []byte
+		path       string
+		wantResult bool
+	}{
+		{
+			name:       "existing shim, same sha256 and path",
+			shimName:   "spin-v1",
+			sha256:     []byte{109, 165, 232, 241, 122, 155, 250, 156, 176, 76, 242, 44, 135, 182, 71, 83, 148, 236, 236, 58, 244, 253, 195, 55, 247, 45, 109, 191, 51, 25, 234, 82},
+			path:       "/opt/kwasm/bin/containerd-shim-spin-v1",
+			wantResult: false,
+		},
+		{
+			name:       "existing shim, different sha256",
+			shimName:   "spin-v1",
+			sha256:     []byte{109, 165, 232, 241, 122, 155, 250, 156, 176, 76, 242, 44, 135, 182, 71, 83, 148, 236, 236, 58, 244, 253, 195, 55, 247, 45, 109, 191, 51, 25, 234, 83},
+			path:       "/opt/kwasm/bin/containerd-shim-spin-v1",
+			wantResult: true,
+		},
+		{
+			name:       "existing shim, different path",
+			shimName:   "spin-v1",
+			sha256:     []byte{109, 165, 232, 241, 122, 155, 250, 156, 176, 76, 242, 44, 135, 182, 71, 83, 148, 236, 236, 58, 244, 253, 195, 55, 247, 45, 109, 191, 51, 25, 234, 82},
+			path:       "/opt/kwasm/bin/containerd-shim-spin-v2",
+			wantResult: true,
+		},
+		{
+			name:       "non-existing shim",
+			shimName:   "non-existing",
+			sha256:     []byte{109, 165, 232, 241, 122, 155, 250, 156, 176, 76, 242, 44, 135, 182, 71, 83, 148, 236, 236, 58, 244, 253, 195, 55, 247, 45, 109, 191, 51, 25, 234, 82},
+			path:       "/opt/kwasm/bin/containerd-shim-spin-v1",
+			wantResult: true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			result := state.ShimChanged(tt.shimName, tt.sha256, tt.path)
+			if result != tt.wantResult {
+				t.Errorf("ShimChanged() = %v, want %v", result, tt.wantResult)
+			}
+		})
+	}
+}
